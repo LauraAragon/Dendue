8cfcacb215ab5ff28b8324ba4df002f95c321c43
LauraAragon <lauraaragon1995@gmail.com>
2015-11-11 17:25:25 -0500

    Arreglado lo del R.

--- app/src/main/java/com/lauraaragon/dendue/ChallengeFiveActivity.java
+++ app/src/main/java/com/lauraaragon/dendue/ChallengeFiveActivity.java
@@ -1,6 +1,10 @@
 package com.lauraaragon.dendue;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,18 +12,35 @@ import android.view.MenuItem;
 
 import com.example.lauraaragon.dendue.R;
 
-public class ChallengeFiveActivity extends Activity {
+public class ChallengeFiveActivity extends Activity implements SensorEventListener {
+
+    private final SensorManager sensorManager;
+    private final Sensor gyroscope;
+
+    public ChallengeFiveActivity(){
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_challenge_five);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_challenge_five, menu);
+        getMenuInflater().inflate(R.menu.menu_challenge_six, menu);
         return true;
     }
 
@@ -36,5 +57,15 @@ public class ChallengeFiveActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
--- app/src/main/java/com/lauraaragon/dendue/ChallengeSixActivity.java
+++ app/src/main/java/com/lauraaragon/dendue/ChallengeSixActivity.java
@@ -1,40 +1,95 @@
 package com.lauraaragon.dendue;
 
 import android.app.Activity;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.lauraaragon.dendue.R;
 
-public class ChallengeSixActivity extends Activity implements SensorEventListener {
+import java.util.ArrayList;
+import java.util.Random;
 
-    private final SensorManager sensorManager;
-    private final Sensor gyroscope;
+import modelo.Letra;
+import modelo.Reto;
 
-    public ChallengeSixActivity(){
-            sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-            gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-    }
+public class ChallengeSixActivity extends Activity {
+
+    private TextView[][] letters;
+    private Reto reto;
+    private int intentosPartida;
+    private int intentosReto;
+    private int indexRandom;
+    private ArrayList<Letra> letras;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_six);
+
+        intentosPartida = 1;
+        intentosReto = 1;
+        indexRandom = 0;
+        reto = getIntent().getExtras().getParcelable("Reto");
+        reto.setIntentos(intentosReto);
+        letras = reto.getPalabra().getLetras();
+
+        letters = new TextView[5][5];
+        for (int i = 0; i<2; i++) {
+            for (int j = 0; j<5; j++) {
+                TextView textView = new Button(this);
+                letters[i][j] = textView;
+            }
+        }
+
+        createTextViews();
     }
 
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_GAME);
+    public void createTextViews(){
+
+        letters[0][0] = (TextView)findViewById(R.id.letterOne);
+        letters[0][1] = (TextView)findViewById(R.id.letterTwo);
+        letters[0][2] = (TextView)findViewById(R.id.letterThree);
+        letters[0][3] = (TextView)findViewById(R.id.letterFour);
+        letters[0][4] = (TextView)findViewById(R.id.letterFive);
+        letters[1][0] = (TextView)findViewById(R.id.letterSix);
+        letters[1][1] = (TextView)findViewById(R.id.letterSeven);
+        letters[1][2] = (TextView)findViewById(R.id.letterEight);
+        letters[1][3] = (TextView)findViewById(R.id.letterNine);
+        letters[1][4] = (TextView)findViewById(R.id.letterTen);
+
+        for (int i = 0; i<2; i++){
+            for (int j = 0; j<5; j++){
+
+                setIForIndexRandom();
+                letters[i][j].setText(letras.get(indexRandom).getLetra());
+
+            }
+        }
     }
 
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
+
+    public void setIForIndexRandom(){
+        int i = 0;
+        Random random = new Random();
+        i = random.nextInt(10)+1;
+
+        if (i==indexRandom){
+            i = random.nextInt(10)+1;
+        }
+
+        setIndexRandom(i);
+
+    }
+
+
+    public void setIndexRandom(int i){
+        this.indexRandom = i;
     }
 
     @Override
@@ -59,13 +114,4 @@ public class ChallengeSixActivity extends Activity implements SensorEventListene
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
 }
